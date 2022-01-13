d12bce8dde1dc63c1f435a58315dcdd220a54558
Olga Dergachyova <olga.dergachyova@clarifai.com>
2022-01-13 15:01:03 +0200

    Increased page limit

--- ias/annotation/describe_annotations.py
+++ ias/annotation/describe_annotations.py
@@ -23,7 +23,7 @@ def get_input_ids(args):
 
   input_ids = {}
   # Get inputs
-  for page in range(1,11):
+  for page in range(1,13):
     list_inputs_response = stub.ListInputs(
                           service_pb2.ListInputsRequest(page=page, per_page=1000),
                           metadata=args.metadata
